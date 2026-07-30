eca113c3c86ddb51dd623069572ac48daded8881
Nigitha Selvan <nigithaselvan@gmail.com>
2026-07-30 20:38:26 +0530

    Use environment variable for API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,30 +1,18 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-    baseURL:"http://localhost:3000/api"
-
+  baseURL:
+    import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
 
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-API.interceptors.request.use((config)=>{
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-
-    const token = localStorage.getItem("token");
-
-
-    if(token){
-
-        config.headers.Authorization = `Bearer ${token}`;
-
-    }
-
-
-    return config;
-
-
+  return config;
 });
-
 
 export default API;
